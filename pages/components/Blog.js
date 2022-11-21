94fa41c7b6197034923a20cd15be8085226609b1
Vincenius <vincent.will@vaitrade.de>
2022-11-21 07:30:29 +0100

    add meta info to head

--- pages/components/Blog.js
+++ pages/components/Blog.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Head from 'next/head'
 import Button from '@mui/material/Button'
 import PostList from './PostList'
 import Header from './Header'
@@ -15,6 +16,12 @@ const Blog = ({ session, name, description, blogId, profileImg, headerImg, useCo
   }
 
   return <div className={styles.container}>
+    <Head>
+      <title>{name} | {description}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <Header name={name} description={description} headerImg={headerImg} />
     <main className={styles.main}>
       { session && <NewPost blogId={blogId} profileImg={profileImg} /> }
