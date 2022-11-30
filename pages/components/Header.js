c66a73853728898257104f4e013f523a1699a8a9
Vincenius <vincent.will@vaitrade.de>
2022-11-30 08:54:58 +0100

    add gradient to header

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,8 +3,15 @@ import styles from './Header.module.css'
 
 const Header = ({ name = '', description = '', headerImg = '', headerColor = 'light' }) => {
   const fontColor = headerColor === 'light' ? '#fafafa' : '#212121'
+  const isCustomHeader = headerImg.includes('https://')
+  const gradient = isCustomHeader
+    ? headerColor === 'light'
+      ? 'linear-gradient(0deg, rgb(0 0 0 / 35%), transparent)'
+      : 'linear-gradient(0deg, rgb(255 255 255 / 35%), transparent)'
+    : 'none'
+
   return <header className={styles.header} style={{ backgroundImage: headerImg, color: fontColor }}>
-    <div className={styles.headerContent}>
+    <div className={styles.headerContent} style={{ background: gradient }}>
       <h1 style={{ color: fontColor }}>{name}</h1>
       <h2 style={{ color: fontColor }}>{description}</h2>
     </div>
